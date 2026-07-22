a73d4350f5ccb6363fcb034cfcef60f71b1f1866
PC <lucaslorran2016@gmail.com>
2026-07-22 12:04:59 -0300

    Utilizando classes Abstrata, Final, Sealed e Non-Sealed

---
+++ get-startded/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main (String[] args){
+
+        Manager manager = new Manager();
+
+        manager.setName("João");
+        manager.setLogin("joao");
+        manager.setPassword("123456");
+
+        System.out.println(manager.getName());
+        System.out.println(manager.getLogin());
+        System.out.println(manager.getPassword());
+    }
+;}
---
+++ get-startded/src/Salesman.java
@@ -0,0 +1,12 @@
+public non-sealed class Salesman extends Employee{
+
+    private double percentForSold;
+
+    public double getPercentForSold() {
+        return percentForSold;
+    }
+
+    public void setPercentForSold(double percentForSold) {
+        this.percentForSold = percentForSold;
+    }
+}
